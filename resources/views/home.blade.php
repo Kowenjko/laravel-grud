40045cc436b5492eda848a205747710fcf601be9
Kowenjko <ortoswt@gmail.com>
2025-11-02 12:43:31 +0200

    add UserController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
-  <h1>Testing 123</h1>
+  <div style="border: 3px solid black;">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+      @csrf
+      <!-- {{ csrf_field() }} -->
+      <input type="text" name="name" placeholder="name">
+      <input type="text" name="email" placeholder="email">
+      <input type="password" name="password" placeholder="password">
+      <button>Register</button>
+    </form>
+  </div>
 </body>
+
 </html>
