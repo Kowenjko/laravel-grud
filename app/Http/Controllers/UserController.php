47690bfcde891dc674f2b3c9a61581003471cc63
Kowenjko <ortoswt@gmail.com>
2025-11-03 10:13:03 +0200

    add auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:10'],
-            'email' => 'required|email',
+            'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => 'required|min:4',
         ]);
 
-        return 'Hello this is UserController';
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        Auth::login($user);
+
+
+        return redirect('/')->with('success', 'Registration successful!');
+    }
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'login_name' => 'required',
+            'login_password' => 'required',
+        ]);
+
+        if (Auth::attempt(['name' => $incomingFields['login_name'], 'password' => $incomingFields['login_password']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You are now logged in!');
+        }
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('success', 'You have been logged out.');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,38 @@
 </head>
 
 <body>
-  <div style="border: 3px solid black;">
+@auth
+    <p>Welcome to Page</p>
+    <form action="/logout" method="POST">
+        @csrf
+       {{ csrf_field() }} 
+        <button>Logout</button>
+    </form>
+@else
+<div style="border: 3px solid black;">
     <h2>Register</h2>
     <form action="/register" method="POST">
       @csrf
-      <!-- {{ csrf_field() }} -->
+       {{ csrf_field() }} 
       <input type="text" name="name" placeholder="name">
       <input type="text" name="email" placeholder="email">
       <input type="password" name="password" placeholder="password">
       <button>Register</button>
     </form>
   </div>
+<div style="border: 3px solid black;">
+    <h2>Login</h2>
+    <form action="/login" method="POST">
+      @csrf
+       {{ csrf_field() }} 
+      <input type="text" name="login_name" placeholder="name">     
+      <input type="password" name="login_password" placeholder="password">
+      <button>Login</button>
+    </form>
+  </div>
+@endauth
+
+  
 </body>
 
 </html>
